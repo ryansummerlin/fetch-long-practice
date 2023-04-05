8fc6c8aded0cb1dbbb98267a45feed99aea17231
ryansummerlin <ryansummerlin777@gmail.com>
2023-04-05 11:39:26 -0700

    Completed all three phases

--- fetch-requests.js
+++ fetch-requests.js
@@ -5,16 +5,46 @@ Paste your code for fetch requests here once you finish each phase.
 
 /* ============================== Phase 1 ============================== */
 
-// Your code here
+fetch('/products', {
+    method: "POST",
+    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+    headers: {"Content-Type": "application/x-www-form-urlencoded"}
+});
 
 
 
 /* ============================== Phase 2 ============================== */
 
-// Your code here
+fetch('/products', {
+    method: "POST",
+    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+    headers: {"Content-Type": "application/x-www-form-urlencoded"}
+})
+    .then(function(res) {
+        console.log(res.status);
+        console.log(res.headers.get("Content-Type"));
+        console.log(res.url);
+        console.log(res.redirected);
+    });
+
 
 
 
 /* ============================== Phase 3 ============================== */
 
-// Your code here
+fetch('/products', {
+    method: 'POST',
+    body: new URLSearchParams({
+        name: "Caribbean Delight Coffee",
+        description: "Made by Manatee Coffee",
+        price: 11.99,
+        categories: "grocery"
+    }),
+    headers: {"Content-Type": "application/x-www-form-urlencoded"}
+})
+    .then(function(res) {
+        console.log(res.status);
+        console.log(res.headers.get("Content-Type"));
+        console.log(res.url);
+        console.log(res.redirected);
+    });
